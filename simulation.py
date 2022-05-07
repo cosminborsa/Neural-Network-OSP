2bbed4d3330afabb873e2f352a0c27fd9e0e578f
Cosmin Borsa <cosmin_borsa@umail.ucsb.edu>
2022-05-07 08:35:37 -0700

    Add files via upload

---
+++ simulation.py
@@ -0,0 +1,53 @@
+## Libraries
+import numpy as np
+
+def sim_gbm(x0, model):
+    '''
+    Simulate paths of Geometric Brownian Motion with constant parameters
+    Simulate from \eqn{p(X_t|X_{t-1})}
+    
+    Parameters
+    ----------
+    x0 : Starting values (matrix of size N x model['dim'])
+    model : Dictionary containing all the parameters, including volatility,
+            interest rate, and continuous dividend yield
+
+    Returns
+    -------
+    A matrix of same dimensions as x0
+    '''
+    length = len(x0)
+    newX = []
+    dt = model['dt']
+    for j in range(model['dim']): # indep coordinates
+       newX.append(x0[:,j]*np.exp(np.random.normal(loc = 0, scale = 1, size = length)*
+                                 model['sigma'][j]*np.sqrt(dt) +
+            (model['r'] - model['div']- model['sigma'][j]**2/2)*dt))
+    return np.reshape(np.ravel(np.array(newX)), (length, model['dim']), order='F')
+
+def stock_sim(nSims, model, start = None):
+    '''
+    Simulates stock paths using Geometric Brownian Motion
+    
+    Parameters
+    ----------
+    nSims : Number of simulations -- N
+    model : Dictionary containing all the parameters, including volatility, 
+            interest rate, and continuous dividend yield
+    start : Optional start value of the stock. The default is N x model['x0'].
+
+    Returns
+    -------
+    An arrayof shape M x N x model['dim']
+    where M is the number of steps model['T']/model['dt']
+    '''
+    if start is None:
+        start = np.reshape(np.repeat(model['x0'], nSims), (nSims, model['dim']))
+    if start.shape != (nSims, model['dim']):
+        start = np.reshape(start, (nSims, model['dim']))
+    nSteps = int(model['T']/model['dt'])
+    test_j = []
+    test_j.append(sim_gbm(start, model))
+    for i in range(1,nSteps):
+        test_j.append(sim_gbm(test_j[i-1], model))
+    return np.reshape(np.ravel(test_j), (nSteps, nSims, model['dim']))
